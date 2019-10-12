07c7039fa3050338770b1734ab01d2b97b48440f
jjwall <jacob.wallace@utexas.edu>
2019-10-11 22:06:19 -0500

    Added lobby server build system

---
+++ webpack-server.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node',
+    entry: './src/lobby/server.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle-server.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    externals: [nodeExternals()],
+};
