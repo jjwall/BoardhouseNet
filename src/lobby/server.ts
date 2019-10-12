e258234803a422677a4e7a439087d39aeb4319ff
jjwall <jacob.wallace@utexas.edu>
2019-10-11 22:21:00 -0500

    Set up initial server code

--- src/lobby/server.ts
+++ src/lobby/server.ts
@@ -1,1 +1,16 @@
-console.log("server files go here");
+import { createServer } from 'http';
+import * as express from 'express';
+
+const app = express();
+const server = createServer(app);
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('./views'));
+
+app.get('/', function(req, res) {
+    res.sendFile('/views/home.html', { root: './'});
+});
+
+server.listen(PORT, function () {
+    console.log(`app listening on port ${PORT}`);
+});
