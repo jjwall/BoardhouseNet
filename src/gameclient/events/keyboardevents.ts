5bcf7831f85e9a284fed0b7bcefefc74345f0240
jjwall <jacob.wallace@utexas.edu>
2021-05-05 22:40:03 -0500

    Set up sendmessages.ts for client

--- src/gameclient/events/keyboardevents.ts
+++ src/gameclient/events/keyboardevents.ts
@@ -1,7 +1,6 @@
-import { MessageTypes } from "../../packets/messagetypes";
-import { Client } from "../client/client";
+import { sendAttackMessage, sendLeftKeyDownMessage, sendLeftKeyUpMessage, sendRightKeyDownMessage, sendRightKeyUpMessage } from "../messaging/sendmessages";
 import { ClientInputMessage } from "../../packets/clientinputmessage";
-import { ClientInputTypes } from "../../packets/clientinputtypes";
+import { Client } from "../client/client";
 
 // keyboard controls
 // visit https://keycode.info/ for other key codes.
@@ -12,128 +11,71 @@ export let handleKeyDownEvent = (client: Client, e: KeyboardEvent) => {
         case 37: // left
         case 65: // a
             if (!client.keyLeftIsDown) {
-                message  = {
-                    messageType: MessageTypes.CLIENT_INPUT_MESSAGE,
-                    inputType: ClientInputTypes.LEFT_KEY_DOWN,
-                    clientId: client.currentClientId
-                }
-
                 client.keyLeftIsDown = true;
-                client.connection.send(JSON.stringify(message));
+                sendLeftKeyDownMessage(client);
             }
             break;
 
         case 39: // right
         case 68: // d
             if (!client.keyRightIsDown) {
-                message = {
-                    messageType: MessageTypes.CLIENT_INPUT_MESSAGE,
-                    inputType: ClientInputTypes.RIGHT_KEY_DOWN,
-                    clientId: client.currentClientId
-                }
-                
                 client.keyRightIsDown = true;
-                client.connection.send(JSON.stringify(message));
+                sendRightKeyDownMessage(client);
             }
             break;
 
         case 38: // up
         case 87: // w
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.up = true;
-            //     }
-            // });
+            // ...
             break;
         
         case 40: // down
         case 83: // s
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.down = true;
-            //     }
-            // });
+            // ...
             break;
 
         case 32: // spacebar
         case 90: // z
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.attack = true;
-            //     }
-            // });
             if (!client.keySpaceIsDown) {
-                let inputMessage: ClientInputMessage = {
-                    messageType: MessageTypes.CLIENT_INPUT_MESSAGE,
-                    inputType: ClientInputTypes.ATTACK,
-                    clientId: client.currentClientId
-                }
-                
                 client.keySpaceIsDown = true;
-                console.log("SPACE - Z - ATTACK");
-                client.connection.send(JSON.stringify(inputMessage));
+                sendAttackMessage(client);
             }
             break;
     }
 }
 
 export function handleKeyUpEvent(client: Client, e: KeyboardEvent) {
-    let message: ClientInputMessage;
+    // let message: ClientInputMessage;
 
     switch(e.keyCode) {
         case 37: // left
         case 65: // a
             if (client.keyLeftIsDown) {
-                message = {
-                    messageType: MessageTypes.CLIENT_INPUT_MESSAGE,
-                    inputType: ClientInputTypes.LEFT_KEY_UP,
-                    clientId: client.currentClientId
-                }
-                
                 client.keyLeftIsDown = false;
-                client.connection.send(JSON.stringify(message));
+                sendLeftKeyUpMessage(client);
             }
-            break;
 
+            break;
         case 39: // right
         case 68: // d
             if (client.keyRightIsDown) {
-                message = {
-                    messageType: MessageTypes.CLIENT_INPUT_MESSAGE,
-                    inputType: ClientInputTypes.RIGHT_KEY_UP,
-                    clientId: client.currentClientId
-                }
-                
                 client.keyRightIsDown = false;
-                client.connection.send(JSON.stringify(message));
+                sendRightKeyUpMessage(client);
             }
-            break;
 
+            break;
         case 38: // up
         case 87: // w
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.up = false;
-            //     }
-            // });
+            // ...
             break;
         
         case 40: // down
         case 83: // s
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.down = false;
-            //     }
-            // });
+            // ...
             break;
 
         case 32: // spacebar
         case 90: // z
-            // state.getEntitiesByKey<Entity>("control").forEach(ent=> {
-            //     if (ent.control) {
-            //         ent.control.attack = false;
-            //     }
-            // });
             if (client.keySpaceIsDown) {
                 client.keySpaceIsDown = false;
             }
