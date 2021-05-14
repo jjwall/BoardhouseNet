2d3b93d71f08db81d9bd3a1f429f09ad2af934e5
jjwall <jacob.wallace@utexas.edu>
2021-05-13 20:09:01 -0500

    check for pushing ent to change list at end of control system to not have redundant change list items

--- src/gameserver/systems/coresystems.ts
+++ src/gameserver/systems/coresystems.ts
@@ -9,33 +9,34 @@ import { sendUpdateEntitiesMessage, sendPlayerAttackAnimDisplayMessage } from ".
  */
 export function controlSystem(ents: ReadonlyArray<Entity>, state: BaseState){
     ents.forEach(ent => {
+        let updatePlayerEnt = false;
         if (ent.control && ent.pos) {
             // Left
             if (ent.control.left) {
                 ent.pos.x -= 25;
 
-                state.server.entityChangeList.push(ent);
+                updatePlayerEnt = true;
             }
 
             // Right
             if (ent.control.right) {
                 ent.pos.x += 25;
 
-                state.server.entityChangeList.push(ent);
+                updatePlayerEnt = true;
             }
 
             // Up
             if (ent.control.up) {
                 ent.pos.y += 25;
 
-                state.server.entityChangeList.push(ent);
+                updatePlayerEnt = true;
             }
 
             // Down 
             if (ent.control.down) {
                 ent.pos.y -= 25;
 
-                state.server.entityChangeList.push(ent);
+                updatePlayerEnt = true;
             }
 
             // Reduce attack cooldown by one tick.
@@ -61,6 +62,13 @@ export function controlSystem(ents: ReadonlyArray<Entity>, state: BaseState){
                 else {
                     ent.control.attack = false;
                 }
+            }
+
+            // Check to see if we need to push changed ent data to change list.
+            // Note: will need to do something akin to this after all systems are run so we don't have
+            // multiple update items to the same ent in the change list for one tick of the engine.
+            if (updatePlayerEnt) {
+                state.server.entityChangeList.push(ent);
             }
         }
     });
