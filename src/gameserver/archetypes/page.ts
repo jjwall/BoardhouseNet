9dc2b4d04ff28ae942e84835d29b93f052542ce7
jjwall <jacob.wallace@utexas.edu>
2021-06-23 10:39:12 -0500

    Added velocity system and component - rdy for use

--- src/gameserver/archetypes/page.ts
+++ src/gameserver/archetypes/page.ts
@@ -1,6 +1,7 @@
 import { ClientEventMessage } from "../../packets/clienteventmessage";
 import { initializeControls } from "../components/initializers";
 import { PositionComponent } from "../components/position";
+import { setVelocity } from "../components/velocity";
 import { Entity } from "../states/gameplay/entity";
 import { BaseState } from "../server/basestate";
 
@@ -8,6 +9,7 @@ export function createPage(state: BaseState, message: ClientEventMessage, pos: P
     let page = new Entity();
     page.player = { id: message.clientId };
     page.pos = pos;
+    page.vel = setVelocity(1);
     page.sprite = { url: "./data/textures/msknight.png", pixelRatio: 4 };
     // page.anim = { sequence: "blah", currentFrame: 0 };
     page.control = initializeControls();
