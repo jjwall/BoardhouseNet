88d03c53b7743673038375ed4cdaf2193eb5a6df
jjwall <jacob.wallace@utexas.edu>
2021-07-21 22:24:45 -0500

    Set up processclientworldmessages.ts

--- src/gameserver/messaging/processclientmessages.ts
+++ src/gameserver/messaging/processclientmessages.ts
@@ -1,17 +1,11 @@
 import { ClientInputTypes, ClientInputMessage, ClientMessageAttack, ClientMessageDownKeyDown, ClientMessageDownKeyUp, ClientMessageLeftKeyDown, ClientMessageLeftKeyUp, ClientMessageRightKeyDown, ClientMessageRightKeyUp, ClientMessageUpKeyDown, ClientMessageUpKeyUp } from "../../packets/clientinputmessage";
 import { Entity } from "../serverengine/entity";
-import { sendCreateEntitiesMessage, sendLoadWorldMessage } from "./sendmessages";
 import { Server } from "../serverengine/server";
 import { MessageTypes } from "../../packets/message";
-import { PlayerClassTypes } from "../../packets/playerclasstypes";
-import { createPage } from "../archetypes/page";
-import { createMagician } from "../archetypes/magician";
-import { createArcher } from "../archetypes/archer";
-import { PositionComponent, setPosition } from "../components/position";
-import { BaseWorldEngine } from "../serverengine/baseworldengine";
 import { QueriedInput } from "../serverengine/interfaces";
 import { ClientWorldMessage, ClientWorldEventTypes, ClientMessagePlayerWorldTransition, ClientMessagePlayerWorldJoin, ClientMessageSpectatorWorldJoin } from "../../packets/clientworldmessage";
 import { PlayerStates } from "../components/player";
+import { processPlayerWorldJoinMessage, processPlayerWorldTransitionMessage, processSpectatorWorldJoinMessage } from "./processclientworldmessages";
 
 export function processClientMessages(server: Server) {
     server.messagesToProcess.forEach(message => {
@@ -42,7 +36,6 @@ function processClientWorldMessages(message: ClientWorldMessage, server: Server)
         // case ClientWorldEventTypes.SPECTATOR_WORLD_TRANSITION:
         //     processSpectatorWorldTransitionMessage(message as ClientMessageSpectatorWorldTransition, server);
         //     break;
-
     }
 }
 
@@ -79,104 +72,6 @@ function processClientInputMessage(message: ClientInputMessage, server: Server) 
             processDownKeyUpMessage(ents, message as ClientMessageDownKeyUp);
             break;
     }
-}
-
-function processPlayerWorldTransitionMessage(message: ClientMessagePlayerWorldTransition, server: Server) {
-    console.log(`(port: ${server.gameServerPort}): client with clientId = "${message.data.clientId}" transitioned as a player with class = "${message.data.playerClass}" in world = "${message.data.newWorldType}"`);
-    let clientWorld: BaseWorldEngine;
-    let playerEnt: Entity;
-
-    try {
-        clientWorld = server.worldEngines.find(worldEngine => worldEngine.worldType === message.data.newWorldType);
-    } catch {
-        throw Error("unable to find world");
-    }
-    
-    switch (message.data.playerClass) {
-        case PlayerClassTypes.PAGE:
-            const pagePos: PositionComponent = setPosition(message.data.newPos.x, message.data.newPos.y, 5);
-            playerEnt = createPage(server, clientWorld, message.data.clientId, pagePos);
-            break;
-        case PlayerClassTypes.MAGICIAN:
-            const magicianPos: PositionComponent = setPosition(message.data.newPos.x, message.data.newPos.y, 5);
-            playerEnt = createMagician(server, clientWorld, message.data.clientId, magicianPos);
-            break;
-        case PlayerClassTypes.ARCHER:
-            const archerPos: PositionComponent = setPosition(message.data.newPos.x, message.data.newPos.y, 5);
-            playerEnt = createArcher(server, clientWorld, message.data.clientId, archerPos);
-            break;
-    }
-
-    // // Not exactly sure why we need this setTimeout here.
-    setTimeout(function() {
-        // Create all entities for connecting client.
-        sendLoadWorldMessage(server, clientWorld.worldLevelData, message.data.clientId);
-        sendCreateEntitiesMessage(clientWorld.getEntitiesByKey<Entity>("global"), server, message.data.newWorldType);
-        playerEnt.player.state = PlayerStates.LOADED;
-    }, 5000);
-}
-
-/**
- * Just because player joins, doesn't mean an ent necessarily needs to be created for them.
- * In this example we do just that.
- * @param message 
- * @param server 
- * @param state 
- */
-export function processPlayerWorldJoinMessage(message: ClientMessagePlayerWorldJoin, server: Server) {
-    console.log(`(port: ${server.gameServerPort}): client with clientId = "${message.data.clientId}" joined as a player with class = "${message.data.playerClass}" in world = "${message.data.worldType}"`);
-    console.log("create player entity");
-    let clientWorld: BaseWorldEngine;
-    let playerEnt: Entity;
-
-    try {
-        clientWorld = server.worldEngines.find(worldEngine => worldEngine.worldType === message.data.worldType);
-    } catch {
-        throw Error("unable to find world");
-    }
-    
-    switch (message.data.playerClass) {
-        case PlayerClassTypes.PAGE:
-            const pagePos: PositionComponent = setPosition(150, 150, 5);
-            playerEnt = createPage(server, clientWorld, message.data.clientId, pagePos);
-            break;
-        case PlayerClassTypes.MAGICIAN:
-            const magicianPos: PositionComponent = setPosition(150, 450, 5);
-            playerEnt = createMagician(server, clientWorld, message.data.clientId, magicianPos);
-            break;
-        case PlayerClassTypes.ARCHER:
-            const archerPos: PositionComponent = setPosition(0, 0, 5);
-            playerEnt = createArcher(server, clientWorld, message.data.clientId, archerPos);
-            break;
-    }
-
-    // // Not exactly sure why we need this setTimeout here.
-    setTimeout(function() {
-        // Create all entities for connecting client.
-        sendLoadWorldMessage(server, clientWorld.worldLevelData, message.data.clientId);
-        sendCreateEntitiesMessage(clientWorld.getEntitiesByKey<Entity>("global"), server, message.data.worldType);
-        playerEnt.player.state = PlayerStates.LOADED;
-    }, 5000);
-
-    // TODO: Loop through NetIdToEnt map and send a bunch of Create Entity messages
-}
-
-// TODO: Give spectators the abilities to control camera / follow players / swap worlds etc.
-function processSpectatorWorldJoinMessage(message: ClientMessageSpectatorWorldJoin, server: Server) {
-    console.log(`(port: ${server.gameServerPort}): client with clientId = "${message.data.clientId}" joined as a spectator in world = "${message.data.worldType}"`);
-    let clientWorld: BaseWorldEngine;
-
-    try {
-        clientWorld = server.worldEngines.find(worldEngine => worldEngine.worldType === message.data.worldType);
-    } catch {
-        throw Error("unable to find world");
-    }
-
-    setTimeout(function() {
-        // Create all entities for connecting client.
-        sendLoadWorldMessage(server, clientWorld.worldLevelData, message.data.clientId);
-        sendCreateEntitiesMessage(clientWorld.getEntitiesByKey<Entity>("global"), server, message.data.worldType);
-    }, 5000);
 }
 
 // TRY TO REDUCE THIS TO BIG O OF N and not N^2
