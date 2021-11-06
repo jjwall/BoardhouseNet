bbe1626fdae774a850d636eeb24cdf2a2078bfe0
jjwall <jacob.wallace@utexas.edu>
2021-11-06 01:11:03 -0500

    added zelfin animations

--- src/gameserver/archetypes/magician.ts
+++ src/gameserver/archetypes/magician.ts
@@ -9,14 +9,15 @@ import { PlayerStates } from "../components/player";
 import { PlayerClassTypes } from "../../packets/enums/playerclasstypes";
 import { SequenceTypes } from "../../modules/animations/sequencetypes";
 import { necroAnim } from "../../modules/animations/animationdata/necro";
+import { zelfinAnim } from "../../modules/animations/animationdata/zelfin"
 
 export function createMagician(server: Server, worldEngine: BaseWorldEngine, clientId: string, pos: PositionComponent): Entity {
     let magician = new Entity();
     magician.player = { id: clientId, state: PlayerStates.UNLOADED, class: PlayerClassTypes.MAGICIAN };
     magician.pos = pos;
     magician.vel = setVelocity(15, 0.5);
-    magician.sprite = { url: "./data/textures/necrowalk1.png", pixelRatio: 8 };
-    magician.anim = { sequence: SequenceTypes.IDLE, blob: necroAnim };
+    magician.sprite = { url: "./data/textures/zelfin001.png", pixelRatio: 8 };
+    magician.anim = { sequence: SequenceTypes.IDLE, blob: zelfinAnim };
     magician.control = setControls();
     magician.hitbox = setHitbox(HitboxTypes.PLAYER, [HitboxTypes.ENEMY], 50, 50, 0, -50);
 
