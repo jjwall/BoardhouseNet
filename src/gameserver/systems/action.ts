0cb222427fbe73fe5016fd0a4409ad3223fc5963
jjwall <jacob.wallace@utexas.edu>
2021-11-27 20:51:40 -0600

    added stub action system function

---
+++ src/gameserver/systems/action.ts
@@ -0,0 +1,6 @@
+import { BaseWorldEngine } from "../serverengine/baseworldengine";
+import { Entity } from "../serverengine/entity";
+
+export function actionSystem(ents: readonly Entity[], worldEngine: BaseWorldEngine) {
+    // ... stud function
+}
