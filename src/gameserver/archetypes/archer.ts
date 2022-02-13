a62ef4ead35512045d83987b80734986efc7d897
Jacob <jacob.wallace@utexas.edu>
2022-02-13 11:36:30 -0600

    Added girl mannequin walk and idle animation sequences for the archer archetype.

--- src/gameserver/archetypes/archer.ts
+++ src/gameserver/archetypes/archer.ts
@@ -1,27 +1,31 @@
+import { initializeSkill, SkillSlotsComponent } from "../components/skillslots";
+import { armatureAnim } from "../../modules/animations/animationdata/armature";
+import { bowAndArrowPress, bowAndArrowRelease } from "../actions/bowandarrow";
+import { PlayerClassTypes } from "../../packets/enums/playerclasstypes";
+import { SequenceTypes } from "../../modules/animations/sequencetypes";
+import { BaseWorldEngine } from "../serverengine/baseworldengine";
 import { setHitbox, HitboxTypes } from "../components/hitbox";
 import { PositionComponent } from "../components/position";
 import { setVelocity } from "../components/velocity";
 import { setMovement } from "../components/movement";
-import { Entity } from "../serverengine/entity";
-import { BaseWorldEngine } from "../serverengine/baseworldengine";
-import { Server } from "../serverengine/server";
 import { PlayerStates } from "../components/player";
-import { PlayerClassTypes } from "../../packets/enums/playerclasstypes";
-import { initializeSkill, SkillSlotsComponent } from "../components/skillslots";
 import { basicSwordAttack } from "../actions/sword";
+import { Entity } from "../serverengine/entity";
+import { Server } from "../serverengine/server";
 
 export function createArcher(server: Server, worldEngine: BaseWorldEngine, clientId: string, pos: PositionComponent): Entity {
     let archer = new Entity();
     archer.player = { id: clientId, state: PlayerStates.UNLOADED, class: PlayerClassTypes.ARCHER };
     archer.pos = pos;
     archer.vel = setVelocity(15, 0.5);
-    archer.sprite = { url: "./data/textures/archer_girl_from_sketch.png", pixelRatio: 1 };
-    // archer.anim = { sequence: "blah", currentFrame: 0 };
+    archer.sprite = { url: "./data/textures/Armature_idle_00.png", pixelRatio: 1 };
+    archer.anim = { sequence: SequenceTypes.IDLE, blob: armatureAnim };
     archer.movement = setMovement();
     archer.hitbox = setHitbox(HitboxTypes.PLAYER, [HitboxTypes.ENEMY], 50, 50, 0, -50);
     archer.skillSlots = new SkillSlotsComponent()
+    archer.skillSlots.setSkillOne(initializeSkill(0, 10, bowAndArrowPress, bowAndArrowRelease, false));
     // archer.skillSlots.setSkillOne(initializeSkill(20, 20, () => { console.log("skilllling")}))
-    archer.skillSlots.setSkillOne(initializeSkill(6, 20, undefined, basicSwordAttack))
+    // archer.skillSlots.setSkillOne(initializeSkill(6, 20, undefined, basicSwordAttack))
 
     worldEngine.registerEntity(archer, server);
 
---
+++ src/modules/animations/animationdata/armature.ts
@@ -0,0 +1,238 @@
+import { AnimationSchema } from "../animationschema";
+import { SequenceTypes } from "../sequencetypes";
+
+export const armatureAnim: AnimationSchema = {
+    [SequenceTypes.IDLE]: [
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_00.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_01.png",
+            nextFrame: 2
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_02.png",
+            nextFrame: 3
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_03.png",
+            nextFrame: 4
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_04.png",
+            nextFrame: 5
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_05.png",
+            nextFrame: 6
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_06.png",
+            nextFrame: 7
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_07.png",
+            nextFrame: 8
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_08.png",
+            nextFrame: 9
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_09.png",
+            nextFrame: 10
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_10.png",
+            nextFrame: 11
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_11.png",
+            nextFrame: 12
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_12.png",
+            nextFrame: 13
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_13.png",
+            nextFrame: 14
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_14.png",
+            nextFrame: 15
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_15.png",
+            nextFrame: 16
+        },
+        {
+            ticks: 8,
+            texture: "./data/textures/Armature_idle_16.png",
+            nextFrame: 0
+        },
+    ],
+    [SequenceTypes.WALK]: [
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_00.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_01.png",
+            nextFrame: 2
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_02.png",
+            nextFrame: 3
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_03.png",
+            nextFrame: 4
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_04.png",
+            nextFrame: 5
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_05.png",
+            nextFrame: 6
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_06.png",
+            nextFrame: 7
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_07.png",
+            nextFrame: 8
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_08.png",
+            nextFrame: 9
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_09.png",
+            nextFrame: 10
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_10.png",
+            nextFrame: 11
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_11.png",
+            nextFrame: 12
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_12.png",
+            nextFrame: 13
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_13.png",
+            nextFrame: 14
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_14.png",
+            nextFrame: 15
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_15.png",
+            nextFrame: 16
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_16.png",
+            nextFrame: 17
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_17.png",
+            nextFrame: 18
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_18.png",
+            nextFrame: 19
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_19.png",
+            nextFrame: 20
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_20.png",
+            nextFrame: 21
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_21.png",
+            nextFrame: 22
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_22.png",
+            nextFrame: 23
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/Armature_walk_23.png",
+            nextFrame: 24
+        },
+        {
+            ticks: 1,
+            texture: "./data/textures/Armature_walk_24.png",
+            nextFrame: 0
+        },
+    ],
+    // [SequenceTypes.ATTACK]: [
+    //     {
+    //         ticks: 10,
+    //         texture: "./data/textures/zelfin007.png",
+    //         nextFrame: 1
+    //     },
+    //     {
+    //         ticks: 10,
+    //         texture: "./data/textures/zelfin008.png",
+    //         nextFrame: 1
+    //     }
+    // ],
+    // [SequenceTypes.ACTION_HOLD]: [
+    //     {
+    //         ticks: 50,
+    //         texture: "./data/textures/zelfin007.png",
+    //         nextFrame: 0
+    //     },
+    // ],
+}
