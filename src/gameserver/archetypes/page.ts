c4a37252074ed5015abaadf46ae108ef47af11c1
Jacob <jacob.wallace@utexas.edu>
2022-12-17 12:05:21 -0800

    pyra walking and idle anims

--- src/gameserver/archetypes/page.ts
+++ src/gameserver/archetypes/page.ts
@@ -7,16 +7,22 @@ import { BaseWorldEngine } from "../serverengine/baseworldengine";
 import { Server } from "../serverengine/server";
 import { PlayerStates } from "../components/player";
 import { PlayerClassTypes } from "../../packets/enums/playerclasstypes";
+import { SequenceTypes } from "../../modules/animations/sequencetypes";
+import { pyraAnim } from "../../modules/animations/animationdata/pyra";
+import { initializeSkill, SkillSlotsComponent } from "../components/skillslots";
+import { basicSwordAttack } from "../actions/sword";
 
 export function createPage(server: Server, worldEngine: BaseWorldEngine, clientId: string, pos: PositionComponent): Entity {
     let page = new Entity();
     page.player = { id: clientId, state: PlayerStates.UNLOADED, class: PlayerClassTypes.PAGE };
     page.pos = pos;
     page.vel = setVelocity(15, 0.5);
-    page.sprite = { url: "./data/textures/msknight.png", pixelRatio: 4 };
-    // page.anim = { sequence: "blah", currentFrame: 0 };
+    page.sprite = { url: "./data/textures/pyra/idle/Armature_idle_00.png", pixelRatio: 1 };
+    page.anim = { sequence: SequenceTypes.IDLE, blob: pyraAnim };
     page.movement = setMovement();
     page.hitbox = setHitbox(HitboxTypes.PLAYER, [HitboxTypes.ENEMY], 128, 128);
+    page.skillSlots = new SkillSlotsComponent();
+    page.skillSlots.setSkillOne(initializeSkill(6, 20, basicSwordAttack, undefined));
 
     worldEngine.registerEntity(page, server);
 
