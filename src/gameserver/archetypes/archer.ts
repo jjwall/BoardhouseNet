bb895f1114281733eaf0525b664fa21b762febb9
Jacob <jacob.wallace@utexas.edu>
2023-02-11 12:43:37 -0800

    ranger idle and walk animations in

--- src/gameserver/archetypes/archer.ts
+++ src/gameserver/archetypes/archer.ts
@@ -1,6 +1,7 @@
 import { initializeSkill, SkillSlotsComponent } from "../components/skillslots";
 import { armatureAnim } from "../../modules/animations/animationdata/armature";
 import { bowAndArrowPress, bowAndArrowRelease } from "../actions/bowandarrow";
+import { rangerAnim } from "../../modules/animations/animationdata/ranger";
 import { PlayerClassTypes } from "../../packets/enums/playerclasstypes";
 import { SequenceTypes } from "../../modules/animations/sequencetypes";
 import { presetInventory } from "../../../database/preset_inventory";
@@ -19,8 +20,8 @@ export function createArcher(server: Server, worldEngine: BaseWorldEngine, clien
     archer.player = setPlayer(clientId, PlayerStates.UNLOADED, PlayerClassTypes.ARCHER, presetInventory);
     archer.pos = pos;
     archer.vel = setVelocity(15, 0.5);
-    archer.sprite = { url: "./data/textures/Armature_idle_00.png", pixelRatio: 1 };
-    archer.anim = { sequence: SequenceTypes.IDLE, blob: armatureAnim };
+    archer.sprite = { url: "./data/textures/ranger/idle/Heroine_ranger_idle_00.png", pixelRatio: 1 };
+    archer.anim = { sequence: SequenceTypes.IDLE, blob: rangerAnim };
     archer.movement = setMovement();
     archer.hitbox = setHitbox(HitboxTypes.PLAYER, [HitboxTypes.ENEMY, HitboxTypes.ITEM_DROP], 50, 50, 0, -50);
     archer.skillSlots = new SkillSlotsComponent()
---
+++ src/modules/animations/animationdata/ranger.ts
@@ -0,0 +1,268 @@
+import { AnimationSchema } from "../animationschema";
+import { SequenceTypes } from "../sequencetypes";
+
+export const rangerAnim: AnimationSchema = {
+    [SequenceTypes.IDLE]: [
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_00.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_01.png",
+            nextFrame: 2
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_02.png",
+            nextFrame: 3
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_03.png",
+            nextFrame: 4
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_04.png",
+            nextFrame: 5
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_05.png",
+            nextFrame: 6
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_06.png",
+            nextFrame: 7
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_07.png",
+            nextFrame: 8
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_08.png",
+            nextFrame: 9
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_09.png",
+            nextFrame: 10
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_10.png",
+            nextFrame: 11
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_11.png",
+            nextFrame: 12
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_12.png",
+            nextFrame: 13
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_13.png",
+            nextFrame: 14
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_14.png",
+            nextFrame: 15
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/idle/Heroine_ranger_idle_15.png",
+            nextFrame: 0
+        },
+    ],
+    [SequenceTypes.WALK]: [
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_00.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_01.png",
+            nextFrame: 2
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_02.png",
+            nextFrame: 3
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_03.png",
+            nextFrame: 4
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_04.png",
+            nextFrame: 5
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_05.png",
+            nextFrame: 6
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_06.png",
+            nextFrame: 7
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_07.png",
+            nextFrame: 8
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_08.png",
+            nextFrame: 9
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_09.png",
+            nextFrame: 10
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_10.png",
+            nextFrame: 11
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_11.png",
+            nextFrame: 12
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_12.png",
+            nextFrame: 13
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_13.png",
+            nextFrame: 14
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_14.png",
+            nextFrame: 15
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_15.png",
+            nextFrame: 16
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_16.png",
+            nextFrame: 17
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_17.png",
+            nextFrame: 18
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_18.png",
+            nextFrame: 19
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_19.png",
+            nextFrame: 20
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_20.png",
+            nextFrame: 21
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_21.png",
+            nextFrame: 22
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_22.png",
+            nextFrame: 23
+        },
+        {
+            ticks: 4,
+            texture: "./data/textures/ranger/walk/Heroine_ranger_run_23.png",
+            nextFrame: 0
+        },
+    ],
+    [SequenceTypes.ATTACK]: [
+        {
+            ticks: 2,
+            texture: "./data/textures/Armature_bow_action_09.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 50,
+            texture: "./data/textures/Armature_bow_action_10.png",
+            nextFrame: 1
+        },
+    ],
+    [SequenceTypes.ACTION_HOLD]: [
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_00.png",
+            nextFrame: 1
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_01.png",
+            nextFrame: 2
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_02.png",
+            nextFrame: 3
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_03.png",
+            nextFrame: 4
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_04.png",
+            nextFrame: 5
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_05.png",
+            nextFrame: 6
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_06.png",
+            nextFrame: 7
+        },
+        {
+            ticks: 6,
+            texture: "./data/textures/Armature_bow_action_07.png",
+            nextFrame: 8
+        },
+        {
+            ticks: 50,
+            texture: "./data/textures/Armature_bow_action_08.png",
+            nextFrame: 8
+        },
+    ],
+}
