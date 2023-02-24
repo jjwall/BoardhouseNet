eca4b330362d23e912cce7a407a37d80e6a88070
Jacob <jacob.wallace@utexas.edu>
2023-02-23 19:13:44 -0800

    page dodegroll and walk anims

--- src/modules/animations/animationdata/page.ts
+++ src/modules/animations/animationdata/page.ts
@@ -87,219 +87,219 @@ export const pageAnim: AnimationSchema = {
     [SequenceTypes.WALK]: [
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_00.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_00.png",
             nextFrame: 1
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_01.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_01.png",
             nextFrame: 2
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_02.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_02.png",
             nextFrame: 3
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_03.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_03.png",
             nextFrame: 4
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_04.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_04.png",
             nextFrame: 5
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_05.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_05.png",
             nextFrame: 6
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_06.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_06.png",
             nextFrame: 7
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_07.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_07.png",
             nextFrame: 8
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_08.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_08.png",
             nextFrame: 9
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_09.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_09.png",
             nextFrame: 10
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_10.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_10.png",
             nextFrame: 11
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_11.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_11.png",
             nextFrame: 12
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_12.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_12.png",
             nextFrame: 13
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_13.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_13.png",
             nextFrame: 14
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_14.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_14.png",
             nextFrame: 15
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_15.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_15.png",
             nextFrame: 16
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_16.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_16.png",
             nextFrame: 17
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_17.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_17.png",
             nextFrame: 18
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_18.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_18.png",
             nextFrame: 19
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_19.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_19.png",
             nextFrame: 20
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_20.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_20.png",
             nextFrame: 21
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_21.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_21.png",
             nextFrame: 22
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_22.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_22.png",
             nextFrame: 23
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/walk/Heroine_run_23.png",
+            texture: "./assets/textures/page/walk/Heroine_page_run_23.png",
             nextFrame: 0
         },
     ],
     [SequenceTypes.DODGE_ROLL]: [
         {
             ticks: 2,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_00.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_00.png",    
             nextFrame: 1
         },
         {
             ticks: 2,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_01.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_01.png",    
             nextFrame: 2
         },
         {
             ticks: 2,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_02.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_02.png",    
             nextFrame: 3
         },
         {
             ticks: 2,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_03.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_03.png",    
             nextFrame: 4
         },
         {
             ticks: 2,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_04.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_04.png",    
             nextFrame: 5
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_05.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_05.png",    
             nextFrame: 6
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_06.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_06.png",    
             nextFrame: 7
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_07.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_07.png",    
             nextFrame: 8
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_08.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_08.png",    
             nextFrame: 9
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_09.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_09.png",    
             nextFrame: 10
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_10.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_10.png",    
             nextFrame: 11
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_11.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_11.png",    
             nextFrame: 12
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_12.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_12.png",    
             nextFrame: 13
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_13.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_13.png",    
             nextFrame: 14
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_14.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_14.png",    
             nextFrame: 15
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_15.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_15.png",    
             nextFrame: 16
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_16.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_16.png",    
             nextFrame: 17
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_17.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_17.png",    
             nextFrame: 18
         },
         {
             ticks: 3,
-            texture: "./assets/textures/knight/dodgeroll/Heroine_dodgeroll_18.png",
+            texture: "./assets/textures/page/dodgeroll/Heroine_page_dodgeroll_18.png",    
             nextFrame: 0
         },
     ],
